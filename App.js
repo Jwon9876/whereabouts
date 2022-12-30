eaf63b78fd9c20a1902ec18b003dac5ff7ae97e2
Jwon <check4545@kakao.com>
2022-12-31 00:37:01 +0900

    Remove Unused

--- App.js
+++ App.js
@@ -1,66 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React, { useEffect, useState } from 'react';
-import {
-	StatusBar,
-	StyleSheet,
-	Text,
-	TextInput,
-	useColorScheme,
-} from 'react-native';
-
-import {
-	Colors,
-	DebugInstructions,
-	Header,
-	LearnMoreLinks,
-	ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React, { useEffect, useState, useRef} from 'react';
+import { Text, TextInput, Image } from 'react-native';
 
 import axios from 'axios';
 import styled from 'styled-components';
 import { API_KEY } from '@env'
 
+const arrivalcode = {
+	"0":"진입",
+	"1":"도착", 
+	"2":"출발", 
+	"3":"전역출발", 
+	"4":"전역진입", 
+	"5":"전역도착", 
+	"99":"운행중"
+}
 
+const metroLine = {
+	"1001": "1호선",
+	"1002": "2호선",
+	"1003": "3호선",
+	"1004": "4호선",
+	"1005": "5호선",
+	"1006": "6호선",
+	"1007": "7호선",
+	"1008": "8호선"
+}
 
+const metroLineColor = {
+	"1호선": "#0052A4",
+	"2호선": "#00A84D",
+	"3호선": "#EF7C1C",
+	"4호선": "#00A5DE",
+	"5호선": "#996CAC",
+	"6호선": "#CD7C2F",
+	"7호선": "#747F00",
+	"8호선": "#E6186C"
+}
 
 const App = () => {
 
 	const [realTimeStationArrival, setRealTimeStationArrival] = useState([]);
 	const [searchWord, setsearchWord] = useState("")
-
-	const arrivalcode = {
-		"0":"진입",
-		"1":"도착", 
-		"2":"출발", 
-		"3":"전역출발", 
-		"4":"전역진입", 
-		"5":"전역도착", 
-		"99":"운행중"
-	}
-
-	const metroLine = {
-		"1001": "1호선",
-		"1002": "2호선",
-		"1003": "3호선",
-		"1004": "4호선",
-		"1005": "5호선",
-		"1006": "6호선",
-		"1007": "7호선",
-		"1008": "8호선"
-	}
-
+	
 	const getRealTimeStationArrival = async (searchWord) => {
 		await axios.get(`http://swopenapi.seoul.go.kr/api/subway/${API_KEY}/json/realtimeStationArrival/0/6/${searchWord}`)
 			.then((response) => {
 				console.log(response.data.realtimeArrivalList)
-				setRealTimeStationArrival(response.data.realtimeArrivalList)
+				const realtimeArrivalList = response.data.realtimeArrivalList;
+				const sortedRealtimeArrivalList = realtimeArrivalList.sort((a, b) => {
+					if(parseInt(a.subwayId) > parseInt(b.subwayId)){
+						return 1;
+					} else{
+						return -1;
+					}
+				})
+				setRealTimeStationArrival(sortedRealtimeArrivalList)
 			})
 			.catch((error) => {
 				console.log(error)
@@ -68,50 +62,47 @@ const App = () => {
 	}
 
 	const expectedArrivalTime = (sec) => {
+		
 		const minute = Math.floor(sec / 60);
 		const seconds = sec % 60;
 		return `${minute}분 ${seconds}초`;
 	}
-
-	useEffect(() => {
-		realTimeStationArrival.sort((a, b) => {
-			if(parseInt(a.subwayId) > parseInt(b.subwayId)){
-				return 1;
-			} else{
-				return -1;
-			}
-		})
-	}, [realTimeStationArrival])
 
 	return (
 		<SafeAreaView>
 			<SearchBar>
 				<TextInput
 					style={TextInputStyle}
-					onChangeText={(e) => setsearchWord(e)}
-					onEndEditing = {() => getRealTimeStationArrival(searchWord)}
+					placeholder={"역 명을 입력하세요."}
+					onChangeText={setsearchWord}
+					onSubmitEditing={() => getRealTimeStationArrival(searchWord)}
 				/>
+				<RefreshBtn>
+					<Image
+						source={require('../whereabouts/Assets/refreshIcon.png')}
+						style={{width: 35, height: 35}}
+					/>
+				</RefreshBtn>
+
 			</SearchBar>
 			<ScrollView>
 			{
 				realTimeStationArrival.map((v, i) => 
-				
 					<View key={i}>
-						<Text> 지하철호선ID: {metroLine[v.subwayId]}</Text>
-						<Text> 상하행선구분: {v.updnLine}</Text>
-						<Text> 도착지방면: {v.trainLineNm}</Text>
-						<Text> 지하철역ID: {v.statnId}</Text>
-						<Text> 지하철역명: {v.statnNm}</Text>
-						<Text> 도착예정열차순번: {v.ordkey}</Text>
-						<Text> 열차도착예정시간: {expectedArrivalTime(v.barvlDt)}</Text>
+						<Text
+							style={{color: metroLineColor[metroLine[v.subwayId]]}}
+						> 
+							{metroLine[v.subwayId]} {v.updnLine} ({v.trainLineNm})
+						</Text>
+						{
+							(v.barvlDt !== "0") ? (<Text> 열차도착예정시간: {expectedArrivalTime(v.barvlDt)}</Text>) : (<></>)
+						}
 						<Text> 열차번호: {v.btrainNo}</Text>
-						<Text> 열차도착정보를 생성한 시각: {v.recptnDt}</Text>
 						<Text> 첫번째도착메세지: {v.arvlMsg2}</Text>
 						<Text> 두번째도착메세지: {v.arvlMsg3}</Text>
 						<Text> 도착코드: {arrivalcode[v.arvlCd]}</Text>
 						<Hr/>
 					</View>
-
 				)
 			}
 			</ScrollView>
@@ -128,15 +119,23 @@ const ScrollView = styled.ScrollView`
 
 const SearchBar = styled.View`
 	border: 1px solid;
+	flex-direction: row;
+	align-items: center;
 `;
 
 const TextInputStyle = {
-	width: "90%",
+	width: "85%",
+	height: 35,
 	borderWidth: 1,
 	borderRadius: 15,
 	marginLeft: 10,
 	padding: 7
 }
+
+const RefreshBtn = styled.TouchableOpacity`
+	position: absolute;
+	right: 10px;
+`;
 
 const Hr = styled.View`
 	width: 100%;
@@ -145,8 +144,7 @@ const Hr = styled.View`
 `;
 
 const View = styled.View`
-
+	margin-top: 10px;
 `;
-
 
 export default App;
