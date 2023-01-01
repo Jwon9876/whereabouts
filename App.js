809f59a1cfd2ba7fb130b0ff76928b5e078cbebb
Jwon <check4545@kakao.com>
2023-01-01 13:53:33 +0900

    Apply Animated Refresh Btn

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Text, TextInput, Image } from 'react-native';
+import { Text, TextInput, Image, Animated, BackHandler } from 'react-native';
 
 import axios from 'axios';
 import styled from 'styled-components';
@@ -41,6 +41,7 @@ const App = () => {
 
 	const [realTimeStationArrival, setRealTimeStationArrival] = useState([]);
 	const [searchWord, setsearchWord] = useState("")
+	const [rotateAnimation, setRotateAnimation] = useState(new Animated.Value(0));
 
 	const getRealTimeStationArrival = async (searchWord) => {
 		await axios.get(`http://swopenapi.seoul.go.kr/api/subway/${API_KEY}/json/realtimeStationArrival/0/6/${searchWord}`)
@@ -48,6 +49,10 @@ const App = () => {
 				console.log(response.data.realtimeArrivalList)
 				const realtimeArrivalList = response.data.realtimeArrivalList;
 				const sortedRealtimeArrivalList = realtimeArrivalList.sort((a, b) => {
+
+
+					// TODO
+					// subwayId 가 시간 순으로 정렬
 					if (parseInt(a.subwayId) > parseInt(b.subwayId)) {
 						return 1;
 					} else {
@@ -68,6 +73,24 @@ const App = () => {
 		return `${minute}분 ${seconds}초`;
 	}
 
+
+	const handleAnimation = () => {
+		Animated.timing(rotateAnimation, {
+			toValue: 1,
+			duration: 2000,
+			useNativeDriver: true
+		}).start(() => {
+			rotateAnimation.setValue(0);
+		});
+	};
+
+
+	const interpolateRotating = rotateAnimation.interpolate({
+		inputRange: [0, 1],
+		outputRange: ['0deg', '720deg'],
+	});
+
+
 	return (
 		<SafeAreaView>
 			<SearchBar>
@@ -78,11 +101,21 @@ const App = () => {
 					onSubmitEditing={() => getRealTimeStationArrival(searchWord)}
 				/>
 				<RefreshBtn
-					onPress = {() => getRealTimeStationArrival(searchWord)}
+					onPress={() => {
+						getRealTimeStationArrival(searchWord),
+							handleAnimation()
+					}
+					}
 				>
-					<Image
+					<Animated.Image
 						source={require('../whereabouts/Assets/refreshIcon.png')}
-						style={{ width: 35, height: 35 }}
+						style={{
+							width: 25, height: 25, transform: [
+								{
+									rotate: interpolateRotating,
+								},
+							],
+						}}
 					/>
 				</RefreshBtn>
 
@@ -123,7 +156,7 @@ const ScrollView = styled.ScrollView`
 `;
 
 const SearchBar = styled.View`
-	border: 1px solid;
+	background-color: #ededed;
 	flex-direction: row;
 	align-items: center;
 `;
